d262e8daacb96d6cc65d9e77220418cd86e94ab1
Akboka Benjamin <benjamin.akboka@ulb.ac.be>
2015-12-16 15:00:40 +0100

    adding the rest of the commentary for the generator class

--- more/Generator.java
+++ more/Generator.java
@@ -2,6 +2,9 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class Generator{
+  /*
+  This class is in charge of the code generation
+  */
   private int ifCounter=0,condCounter=0,endCounter=0,loopCount=0,endLoopCount=0;
   private int orCounter=0;
   private PrintWriter writer; // the llvm code stored
@@ -78,24 +81,39 @@ public class Generator{
     condCounter+=1;
   }
   public void ifblock(){
+    /*
+    Print the header for an if block
+    */
     jump(false);
     writer.println("	if_true"+ifCounter+":");
     ifCounter+=1;
   }
   public void endIf(){
+    /*
+    Print the footer for an if block
+    */
     if(ifCounter == endCounter){
 			writer.println("		br label %end"+(ifCounter-1));
 			writer.println("	end"+(ifCounter-1)+":");
 		}
   }
   public void elseIf(){
+    /*
+      Print the footer for an if block in case of an else
+    */
     writer.println("		br label %end"+(ifCounter-1));
 		writer.println("	else"+(ifCounter-1)+":");
   }
   public void incrementEnd(){
+    /*
+    increment the number of ending loop
+    */
     endCounter+=1;
   }
   public void jump(boolean loop){
+    /*
+    Print the jump code. Jump to a loop block in case of loop or if block otherwise
+    */
     if(loop)
       writer.println("		br i1 "+stack.get(stack.size()-1)+", label %"+"loop"+loopCount+", label %endLoop"+loopCount);
     else
@@ -103,6 +121,10 @@ public class Generator{
     stack.remove(stack.size()-1);
   }
   public void or(boolean loop){
+    /*
+      Print the or blocks. The jump in case of false need to be to a or block
+      in order to test more conditions
+    */
     if(loop)
       writer.println("		br i1 "+stack.get(stack.size()-1)+", label %"+"loop"+loopCount+", label %or"+orCounter);
     else
@@ -112,6 +134,10 @@ public class Generator{
     stack.remove(stack.size()-1);
   }
   public void and(){
+    /*
+    Print the code condition concerning the and. This function will generate
+    condition matching the other one two by two
+    */
     writer.println("		%cond"+condCounter+" = icmp eq i1 "+stack.get(stack.size()-1)+","+stack.get(stack.size()-2));
     stack.remove(stack.size()-1);
     stack.remove(stack.size()-1);
@@ -119,12 +145,18 @@ public class Generator{
     condCounter+=1;
   }
   public void beginDo(){
+    /*
+    Print the header for a do block. This print the jump and the block
+    */
     writer.println("		br i1 "+stack.get(stack.size()-1)+", label %loop"+loopCount+", label %endLoop"+loopCount);
     writer.println("	loop"+loopCount+":");
 		loopCount+=1;
     stack.remove(stack.size()-1);
   }
   public void od(){
+    /*
+    Print the footer for a do block. This print the jump and the block
+    */
     writer.println("		br i1 "+stack.get(stack.size()-1)+", label %loop"+(loopCount-endLoopCount-1)+", label %endLoop"+(loopCount-endLoopCount-1));
     writer.println("	endLoop"+(loopCount-endLoopCount-1)+":");
     stack.remove(stack.size()-1);
@@ -133,9 +165,15 @@ public class Generator{
       endLoopCount = 0; // so we reset the value
   }
   public void read(String var){
+    /*
+    Print the code for a read
+    */
     writer.println("		"+var+" = call i32 @getchar()");
   }
   public void print(String toShow){
+    /*
+    Print the code for a print
+    */
     writer.println("		call i32 @putchar(i32 "+toShow+")");
   }
 }
