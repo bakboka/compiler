b9fbf1d06163913d8aa3dbf98b817aae97d042b2
Akboka Benjamin <benjamin.akboka@ulb.ac.be>
2015-12-17 10:58:02 +0100

    adding comments. Changed the privacy settings.

--- more/Main.java
+++ more/Main.java
@@ -5,12 +5,12 @@ import java.util.Collections;
 
 public class Main {
 
-	private static ArrayList<Symbol> listSym = new ArrayList<Symbol>();
+	private static ArrayList<Symbol> listSym = new ArrayList<Symbol>(); //list of symbol
 	private static int curToken = 0;
-	private static Generator code;
+	private static Generator code; //code generator
 	private static ArrayList<String> stack = new ArrayList<String>(); //stack of variables
 	private static ArrayList<Symbol> id = new ArrayList<Symbol>(); //table of symbol
-	private static int unnamedVariable = 0,and=0;
+	private static int unnamedVariable = 0,andCounter=0;
 
 	public static void main(String[] args) throws FileNotFoundException,IOException,ParseError{
 		FileReader reader = new FileReader(args[0]);
@@ -32,31 +32,44 @@ public class Main {
 		checks if the identifier is already in our list
 		returns true if it is
 		return false otherwise
+
+		Arguments : Symbol symbol
 	*/
 		for(int i=0;i<id.size();i++){
-			if(id.get(i).getValue().toString().equals(symbol.getValue().toString())){
+			if(id.get(i).getValue().toString().equals(symbol.getValue().toString()))
 				return true;
-			}
 		}
 		return false;
 	}
 	public static void Parser() throws ParseError{
+		/*
+		Parse the input code. Throw an error if an unexpected token is find
+
+		Arguments : none
+		Return : none
+		*/
 			if(!Goal())
 				throw new ParseError(listSym.get(curToken));
 	}
-	public static boolean Match(LexicalUnit t){
+	private static boolean Match(LexicalUnit t){
+		/*
+		try to match the current token with what is expected. Return true if it
+		is matched
+
+		Arguments : LexicalUnit token
+		Return : boolean
+		*/
 		if (listSym.get(curToken).getType().equals(t)){
 			curToken++;
 			return true;
 		}
-		else{
+		else
 			return false;
-		}
 	}
-	public static void Send_output(int no){
+	private static void Send_output(int no){
 		//System.out.print(no + " ");
 	}
-public static void writeCond(Condition cond,boolean loop,boolean repeat){
+	private static void writeCond(Condition cond,boolean loop,boolean repeat){
 	/*
 	write all the conditions contained by the object Condition
 
@@ -64,46 +77,55 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 	Return : none
 	*/
 	ArrayList<String> temp = new ArrayList<String>();
-	int i=0;
-	int num=0;
-	if(repeat) // if it is a repeat then we need to inverse the stack
+	int i=0; //counter for the fifo from the stack
+	int num=0; //number of "and"
+	if(repeat) // if it is a repeat then we need to inverse the stack to get a fifo
 		num = cond.getAnd();
 	while(cond.moreCond()){ // until no more conditions
 		temp = cond.getCond(); // get the condition
 		if(temp.get(0).equals("or"))
 			code.or(loop,repeat); // print an or block
 		else if (temp.get(0).equals("and"))
-			code.and(); // generate the and condition
+			code.and(); // generate the and conditions
 		else if(!repeat)
 			code.cond(temp.get(1),temp.get(0),temp.get(2)); //original condition
 		else{ // repeated condition
-			if(stack.size()>0){ // we compare a variable
-				if(num!=0){ // because it is a stack, we need to do a fifo
-					code.load("%"+unnamedVariable,stack.get(stack.size()-num+i)); //loading the variable to compare
-					stack.remove(stack.size()-num+i);
-				}
-				else{ // normal case
-					code.load("%"+unnamedVariable,stack.get(stack.size()-1)); //loading the variable to compare
-					stack.remove(stack.size()-1);
-				}
-				code.cond(temp.get(1),"%"+unnamedVariable,temp.get(2));
-				unnamedVariable+=1;
-			}
-			else // we compare two int
-				code.cond(temp.get(1),temp.get(0),temp.get(2));
+			writeRepeatedCond(num,i,temp);
 		}
 		i++;
 	}
 	cond.resetPointer();
 }
-	public static boolean Goal(){
+	private static void writeRepeatedCond(int num,int i, ArrayList<String> temp){
+		/*
+		write the conditions that appears at the end of a loop
+
+		Arguments : int num, int i, ArrayList temp
+		Return : none
+		*/
+		if(stack.size()>0){ // we compare a variable
+			if(num!=0){ // we need to do a fifo of the variables. The stack have the reverse order
+				code.load("%"+unnamedVariable,stack.get(stack.size()-num+i)); //loading the variable to compare
+				stack.remove(stack.size()-num+i);
+			}
+			else{ // normal case ie. one condition
+				code.load("%"+unnamedVariable,stack.get(stack.size()-1)); //loading the variable to compare
+				stack.remove(stack.size()-1);
+			}
+			code.cond(temp.get(1),"%"+unnamedVariable,temp.get(2));
+			unnamedVariable+=1;
+		}
+		else // we compare two int
+			code.cond(temp.get(1),temp.get(0),temp.get(2));
+	}
+	private static boolean Goal(){
 		Send_output(1);
 		if(Program())
 		if(Match(LexicalUnit.END_OF_STREAM))
 			return true;
 		return false;
 	}
-	public static boolean Program(){
+	private static boolean Program(){
 		Send_output(2);
 		if(Match(LexicalUnit.BEG))
 		if(Code())
@@ -111,7 +133,7 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 			return true;
 		return false;
 	}
-	public static boolean Code(){
+	private static boolean Code(){
 		switch(listSym.get(curToken).getType()){
 		case OD:
 		case FI:
@@ -131,14 +153,14 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 		}
 		return false;
 	}
-	public static boolean InstList(){
+	private static boolean InstList(){
 		Send_output(5);
 		if(Instruction())
 		if(EndList())
 			return true;
 		return false;
 	}
-	public static boolean EndList(){
+	private static boolean EndList(){
 		switch(listSym.get(curToken).getType()){
 		case SEMICOLON:
 			Send_output(6);
@@ -155,7 +177,7 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 		}
 		return false;
 	}
-	public static boolean Instruction(){
+	private static boolean Instruction(){
 		switch(listSym.get(curToken).getType()){
 			case VARNAME:
 				Send_output(8);
@@ -190,7 +212,7 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 		}
 		return false;
 	}
-	public static boolean Assign(){
+	private static boolean Assign(){
 		Send_output(14);
 		if(Match(LexicalUnit.VARNAME)){
 			stack.add("%"+(String)listSym.get(curToken-1).getValue());
@@ -205,22 +227,22 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 		return false;
 
 	}
-	public static boolean ExprArith(boolean loop){
+	private static boolean ExprArith(boolean loop){
 		Send_output(15);
 		if(Factor(loop))
 		if(Terms())
 			return true;
 		return false;
 	}
-	public static boolean Terms(){
+	private static boolean Terms(){
 		switch(listSym.get(curToken).getType()){
 		case PLUS:
 		case MINUS:
 			Send_output(17);
-			ArrayList<Boolean> test = AddSub();
-			if(test.get(0))
+			ArrayList<Boolean> op = AddSub();
+			if(op.get(0))
 			if(ExprArith(false)){
-				code.addition("%"+unnamedVariable,stack.get(stack.size()-2),stack.get(stack.size()-1),test.get(1));
+				code.addition("%"+unnamedVariable,stack.get(stack.size()-2),stack.get(stack.size()-1),op.get(1));
 				stack.remove(stack.size()-1); // poping the variable. It is useless now
 				stack.remove(stack.size()-1); // poping the variable. It is useless now
 				stack.add("%"+unnamedVariable);
@@ -251,14 +273,13 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 		}
 		return false;
 	}
-	public static boolean A1(boolean loop){
+	private static boolean A1(boolean loop){
 		switch(listSym.get(curToken).getType()){
 		case VARNAME:
 			Send_output(22);
 			if(Match(LexicalUnit.VARNAME)){
-				if(loop){
-					stack.add("%"+(String)listSym.get(curToken-1).getValue());
-				}
+				if(loop)
+					stack.add("%"+(String)listSym.get(curToken-1).getValue()); //we need to remember to regenerate condition for loops
 				code.load("%"+unnamedVariable,"%"+(String)listSym.get(curToken-1).getValue());
 				stack.add("%"+unnamedVariable);
 				unnamedVariable+=1;
@@ -287,7 +308,7 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 		}
 		return false;
 	}
-	public static ArrayList<Boolean> AddSub(){
+	private static ArrayList<Boolean> AddSub(){
 		ArrayList<Boolean> sub = new ArrayList<Boolean>();
 		switch(listSym.get(curToken).getType()){
 		case PLUS:
@@ -309,22 +330,22 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 		sub.add(false);//nothing matched
 		return sub;
 	}
-	public static boolean Factor(boolean loop){
+	private static boolean Factor(boolean loop){
 		Send_output(18);
 		if(A1(loop))
 		if(Factors(loop))
 			return true;
 		return false;
 	}
-	public static boolean Factors(boolean loop){
+	private static boolean Factors(boolean loop){
 		switch(listSym.get(curToken).getType()){
 		case TIMES:
 		case DIVIDE:
 			Send_output(20);
-			ArrayList<Boolean> test = MultiDiv();
-			if(test.get(0))
+			ArrayList<Boolean> op = MultiDiv();
+			if(op.get(0))
 			if(Factor(loop)){
-				code.multiply("%"+unnamedVariable,stack.get(stack.size()-2),stack.get(stack.size()-1),test.get(1));
+				code.multiply("%"+unnamedVariable,stack.get(stack.size()-2),stack.get(stack.size()-1),op.get(1));
 				stack.remove(stack.size()-1); // poping the variable. It is useless now
 				stack.remove(stack.size()-1); // poping the variable. It is useless now
 				stack.add("%"+unnamedVariable);
@@ -364,7 +385,7 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 		}
 		return false;
 	}
-	public static ArrayList<Boolean> MultiDiv(){
+	private static ArrayList<Boolean> MultiDiv(){
 		ArrayList<Boolean> div = new ArrayList<Boolean>();
 		switch(listSym.get(curToken).getType()){
 		case TIMES:
@@ -386,7 +407,7 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 		div.add(false); // nothing was matched
 		return div;
 	}
-	public static Condition Cond(boolean loop){
+	private static Condition Cond(boolean loop){
 		Condition listCond = new Condition();
 		Send_output(30);
 		if(B1(listCond,loop))
@@ -397,7 +418,7 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 		listCond.setMatched(false); //nohing matched
 		return listCond;
 	}
-	public static boolean B1(Condition cond,boolean loop){
+	private static boolean B1(Condition cond,boolean loop){
 		switch(listSym.get(curToken).getType()){
 		case VARNAME:
 		case NUMBER:
@@ -415,7 +436,7 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 		}
 		return false;
 	}
-	public static boolean B2(Condition cond,boolean loop){
+	private static boolean B2(Condition cond,boolean loop){
 		switch(listSym.get(curToken).getType()){
 		case OR:
 			Send_output(33);
@@ -445,7 +466,7 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 		}
 		return false;
 	}
-	public static boolean And(Condition oldCond,boolean loop){
+	private static boolean And(Condition oldCond,boolean loop){
 		switch(listSym.get(curToken).getType()){
 		case AND:
 			Send_output(35);
@@ -470,7 +491,7 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 		}
 		return false;
 	}
-	public static boolean SimpleCond(Condition cond,boolean loop){
+	private static boolean SimpleCond(Condition cond,boolean loop){
 		Send_output(37);
 		if(ExprArith(loop)){
 			cond.addCond(stack.get(stack.size()-1)); //add the result of the expr
@@ -482,14 +503,14 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 					cond.addCond(stack.get(stack.size()-1));//add the result of the expr
 					stack.remove(stack.size()-1);
 					if(loop)
-						and+=1;
+						andCounter+=1; // one more condition
 					return true;
 				}
 			}
 		}
 		return false;
 	}
-	public static boolean If(){
+	private static boolean If(){
 		Send_output(38);
 		if(Match(LexicalUnit.IF)){
 			Condition cond = Cond(false);//not a loop
@@ -506,7 +527,7 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 		}
 		return false;
 	}
-	public static boolean EndIf(){
+	private static boolean EndIf(){
 		switch(listSym.get(curToken).getType()){
 		case FI:
 			Send_output(39);
@@ -529,7 +550,7 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 		}
 		return false;
 	}
-	public static boolean Comp(){
+	private static boolean Comp(){
 		switch(listSym.get(curToken).getType()){
 		case EQUAL:
 			Send_output(41);
@@ -575,14 +596,14 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 		}
 		return false;
 	}
-	public static boolean While(){
+	private static boolean While(){
 		Send_output(47);
 		if(Match(LexicalUnit.WHILE)){
 			Condition cond = Cond(true);//a loop
 			if(cond.getMatched())
 			if(Match(LexicalUnit.DO)){
-				cond.setAnd(and); // keep in memory
-				and=0; //reset
+				cond.setAnd(andCounter); // keep the number of cond in memory
+				andCounter=0; //reset
 				writeCond(cond,true,false);//a loop, not a repeated cond
 				code.beginDo();
 				if(Code())
@@ -595,7 +616,7 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 		}
 		return false;
 	}
-	public static boolean For(){
+	private static boolean For(){
 		Send_output(48);
 		Condition cond = new Condition();
 		if(Match(LexicalUnit.FOR))
@@ -603,13 +624,13 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 			stack.add("%"+(String)listSym.get(curToken-1).getValue());
 			if(Match(LexicalUnit.FROM))
 			if(ExprArith(false)){
-				code.assign(stack.get(stack.size()-2),stack.get(stack.size()-1));
+				code.assign(stack.get(stack.size()-2),stack.get(stack.size()-1));//store the expr into the var
 				stack.remove(stack.size()-1); //only one time because we need to remember the var name
 				if(Match(LexicalUnit.BY))
 				if(ExprArith(false))
 				if(Match(LexicalUnit.TO))
 				if(ExprArith(false)){
-					beginFor();
+					beginFor(); //allocate and store variables for the loop
 					cond.addCond(stack.get(stack.size()-1));
 					stack.remove(stack.size()-1);
 					//stack = varname,to,by,unnamed
@@ -621,7 +642,7 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 					code.beginDo();
 					if(Match(LexicalUnit.DO))
 					if(Code()){
-						endFor();
+						endFor(); // write the condition to stay in the loop
 						writeCond(cond,true,true); //a loop, a repeated cond
 						code.od();
 						if(Match(LexicalUnit.OD))
@@ -632,7 +653,7 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 		}
 		return false;
 	}
-	public static void beginFor(){
+	private static void beginFor(){
 		/*
 		allocate and store the variables needed to execute the for loop
 
@@ -660,7 +681,7 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 		//stack = varname,to,by,unnamed,unnamed
 		unnamedVariable+=1;
 	}
-	public static void endFor(){
+	private static void endFor(){
 		/*
 		Load and do the arithmetics necessary to execute the next for loop
 
@@ -679,7 +700,7 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 		code.assign(stack.get(stack.size()-1),"%"+unnamedVariable);//store the new value of var
 		unnamedVariable+=1;
 	}
-	public static boolean Print(){
+	private static boolean Print(){
 		Send_output(50);
 		if(Match(LexicalUnit.PRINT))
 		if(Match(LexicalUnit.LEFT_PARENTHESIS))
@@ -692,7 +713,7 @@ public static void writeCond(Condition cond,boolean loop,boolean repeat){
 		}
 		return false;
 	}
-	public static boolean Read(){
+	private static boolean Read(){
 		Send_output(51);
 		if(Match(LexicalUnit.READ))
 		if(Match(LexicalUnit.LEFT_PARENTHESIS))
