6c1cd5e4868bd646065f381d815a7721c3c731e2
Akboka Benjamin <benjamin.akboka@ulb.ac.be>
2015-12-18 13:30:40 +0100

    Changed the error explanation

--- more/ParseError.java
+++ more/ParseError.java
@@ -1,6 +1,15 @@
 class ParseError extends Exception{
+  /**
+   * handles the error when parsing
+   */
   public ParseError(Symbol expected){
+    /**
+     * Generate an error explaining what went wrong
+     *
+     * @param expected the symbol expected by the parser
+     */
     System.out.println();
-    System.out.println("Unexpected symbol : "+expected.getType()+" at line : "+expected.getLine());
+    System.out.println("Unexpected symbol at line : "+expected.getLine()+" column : "+expected.getColumn());
+    System.out.println("Expected symbol : "+expected.getType());
   }
 }
